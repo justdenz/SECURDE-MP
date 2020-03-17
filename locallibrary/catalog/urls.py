1d8457145a6312c91a82f3873208d8cae9f43fe7
Denzel Co <denz3550@gmail.com>
2020-03-17 22:47:52 +0800

    change framework

--- locallibrary/catalog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns =[
-    path('', views.index, name='index'),
-    path('books/', views.BookListView.as_view(), name='books'),
-    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
-    path('', views.signup, name='signup'),
-]
