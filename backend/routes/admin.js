c66b27203c13a996f2c449baf13e71c62572914a
Denzel Co <denz3550@gmail.com>
2020-03-30 20:27:36 +0800

    fixed routes

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -3,12 +3,42 @@ const session = require("express-session")
 const bodyparser = require('body-parser')
 const router = express.Router()
 
+const {
+    ValidateCreateUser,
+    ValidateGetUserByRole
+} = require('../Services/User_Service')
+
 router.use(function timeLog (req, res, next) {
     next()
 })
 
 router.get('/', (req, res) => {
     res.send("API(ADMIN) is wokring properly")
+})
+
+router.get('/get_all_manager', async (req, res) => {
+    let result = ValidateGetUserByRole("MANAGER")
+    res.send({
+        status: result.status,
+        payload: result.payload
+    })
+})
+
+router.post("/create_manager", async (req, res) => {
+    var user_id = req.body.id_number
+    var first_name = req.body.first_name
+    var last_name = req.body.last_name
+    var username = req.body.username
+    var password = req.body.password
+    var email = req.body.email
+    var role_name = "MANAGER"
+
+    const result = await ValidateCreateUser(user_id, first_name, last_name, username, password, email, role_name)
+
+    res.send({
+        status: result.status,
+        payload: result.payload
+    })
 })
 
 module.exports = router
