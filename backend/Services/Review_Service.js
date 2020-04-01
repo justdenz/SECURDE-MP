fde808c7f03660a0fc2cc39795327cb218c60d9b
rebecabecalyn <45415030+rebecabecalyn@users.noreply.github.com>
2020-04-01 23:33:10 +0800

    Revert "Revert "create review action""
    This reverts commit 2cc7ddb3ee38816f1b031020c9b6200cf6ec4229.

--- backend/Services/Review_Service.js
+++ backend/Services/Review_Service.js
@@ -1,7 +1,13 @@
 const {CreateReview, DeleteReview, GetAllReviewsByUser, GetAllReviewsByBook} = require('./Review_DB.js')
+const{ReviewBook} = require("./UserAction_DB.js")
 
 async function ValidateCreateReview(book_id, user_id, comment){
   await CreateReview(book_id, user_id, comment)
+
+  /*User Action*/
+  await ReviewBook(user_id, book_id)
+  .then(console.log("Action logged as Review Book"))
+  .catch(err => console.log(err))
 }
 
 async function ValidateDeleteReview(review_id){
