99fb03746db631af5e47946b9438406c179c8410
schuyl3r <schuyler_ng@dlsu.edu.ph>
2020-04-19 15:16:51 +0800

    feat: added encryption in local storage

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -4,9 +4,17 @@ import createIdleMonitorReduxMiddleware from 'redux-idle-timeout-monitor';
 import rootReducer from './reducers/rootReducer';
 import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
+import createEncryptor from 'redux-persist-transform-encrypt'
 
+const encryptor = createEncryptor({
+  secretKey: 'my-super-secret-key',
+  onError: function(error) {
+    console.log("Error in Storage Encryption: ",  error);
+  }
+})
 // configure middleware for 10 minute timeout
 const idleMonitorMiddleware = createIdleMonitorReduxMiddleware(10 * 60 * 1000, autoLogout);
+
 
 export function autoLogout() {
   alert("Idle time reached, you've been logged out!")
@@ -18,6 +26,7 @@ export function autoLogout() {
 const persistConfig = {
   key: 'root',
   storage,
+  transforms: [encryptor]
 }
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)
